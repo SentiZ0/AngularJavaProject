a534e2cd2cddd205c1e2a601b67aab493b420248
Jakub Kirlowicz <80446@student.pb.edu.pl>
2023-06-22 16:49:25 +0200

    First service and Form

---
+++ src/app/components/category/category-create/category-create.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Category } from 'src/app/models/category';
+import { categoryService } from 'src/app/services/category-service.service';
+
+@Component({
+  selector: 'app-category-create',
+  templateUrl: './category-create.component.html',
+  styleUrls: ['./category-create.component.css']
+})
+export class CategoryCreateComponent {
+  categoryName: string = "";
+  
+  constructor(private categoryService: categoryService)
+  {}
+
+  createCategory() {
+    let id = 0;
+  
+    let category: Category = new Category(id, this.categoryName);
+  
+    this.categoryService.createCategory(category).subscribe();
+  }
+}
--- src/app/services/category-service.service.ts
+++ src/app/services/category-service.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Category } from '../models/category';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CategoryServiceService {
+export class categoryService {
 
-  constructor() { }
+  constructor(private httpClient : HttpClient) { }
+
+  getCategories = (): Observable<Category> => this.httpClient.get<Category>("https://localhost:8080/category/Comments");
+
+  getCategory  = (id: number): Observable<Category> => this.httpClient.get<Category>("https://localhost:8080/category/Comments/" + id);
+
+  createCategory = (category: Category): Observable<Category> => this.httpClient.post<Category>("https://localhost:8080/category/Comments", category);
+
+  updateCategory = (id: number, category: Category): Observable<Category> => this.httpClient.put<Category>("https://localhost:8080/category/Comments/" + id, category);
+
+  deleteCategory = (id: number): Observable<Category> => this.httpClient.delete<Category>("https://localhost:8080/category/Comments/" + id);
 }
